d7913afe4a53dcfd26c869512d7d7e8ec721a053
SHARAFELDIN KABASHI <74854142+sharafeldinkabashi@users.noreply.github.com>
2020-12-28 02:38:41 +0800

    Create submit interface
     addition of  submit interface for question function for the event organizer

--- view/EventOrganizerInterface/submitInterface.php
+++ view/EventOrganizerInterface/submitInterface.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Title -->
-    <title>THANK YOU</title>
+    <title>THANK YOU VERY MUCH</title>
 
     <!-- Favicon -->
     <link rel="icon" href="../../libs/img/core-img/favicon1.ico">
